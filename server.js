77a519ae2abee3fa70dcfef8c7471fc7186fc600
Gokul s <samigokul474@gmail.com>
2021-04-11 20:57:14 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const app = express();
+const cors = require("cors");
+const mongodb = require("mongodb");
+app.use(cors())
+app.use(express.json())
+app.listen(process.env.port || 4000);
+
+const URL = "mongodb://localhost:27017";
+const DB = "shorterlink"
+app.post("/",async (req,res)=>{
+    try{
+        console.log(req.body);
+    let connection = await mongodb.connect(URL);
+    let db = connection.db(DB);
+     await db.collection("link").insertOne(req.body);
+    connection.close();
+    res.json({
+        "message":"added"
+    })}
+    catch(error)
+    {
+        console.log(error);
+    }
+})
+
+app.get("/:id", async (req,res)=>{
+    try{
+        console.log(req.body);
+    let connection = await mongodb.connect(URL);
+    let db = connection.db(DB);
+    let link =  await db.collection("link").find({shortid:req.params.id}).toArray();
+    connection.close();
+    res.send(link);}
+    catch(error)
+    {
+        console.log(error);
+    }
+})
+app.get("/", async (req,res)=>{
+    try{
+        console.log(req.body);
+    let connection = await mongodb.connect(URL);
+    let db = connection.db(DB);
+    let link =  await db.collection("link").find().toArray();
+    connection.close();
+    res.send(link);}
+    catch(error)
+    {
+        console.log(error);
+    }
+})
+app.delete("/:id",async (req,res)=>{
+    try{
+        console.log(req.body);
+    let connection = await mongodb.connect(URL);
+    let db = connection.db(DB);
+    let link =  await db.collection("link").deleteOne({shortid:req.params.id});
+    connection.close();
+    res.json({
+        "Link":"removed"
+    })}
+    catch(error)
+    {
+        console.log(error);
+    }  
+})
